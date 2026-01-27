c78cabc28c2edc0aa61d9c0eeaab9029b873cb49
holpunk <aqshey@gmail.com>
2026-01-27 22:38:54 +0100

    refactoring

--- terraform/modules/aws_foundation/main.tf
+++ terraform/modules/aws_foundation/main.tf
@@ -1,14 +1,3 @@
-variable "environment" {
-  description = "Deployment environment (e.g., dev, prod)"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -77,10 +66,3 @@ resource "aws_security_group" "default_deny" {
   }
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "private_subnets" {
-  value = aws_subnet.private[*].id
-}
